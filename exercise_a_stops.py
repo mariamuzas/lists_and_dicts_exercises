7770df01799f7c1a18bbcdca6302e20af98c4ffc
yanazykova <yvz1973@gmail.com>
2020-09-09 14:15:47 +0100

    Removed Livingston

--- exercise_a_stops.py
+++ exercise_a_stops.py
@@ -14,9 +14,13 @@ stops.insert(4, "Polmont")
 
 #4. Print out the index position of "Linlithgow"
 
-print(stops.index("Linlithgow"))
+print(stops.index("Linlithgow"))    
 
 #5. Remove "Livingston" from the list using its name
+
+stops.remove("Livingston")
+print(stops)
+
 #6. Delete "Cumbernauld" from the list by index
 #7. Print the number of stops there are in the list
 #8. Sort the list alphabetically
